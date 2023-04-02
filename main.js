7ba440f81e8e40e72489bb2011f3951aa2fc30e5
Alan dev <Alan.jimenez353@gmail.com>
2023-04-02 20:55:21 -0300

    Position fixed

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 })
 const torus = new THREE.Mesh(geometry, material);
 
 
-scene.add(torus)
+//scene.add(torus)
 
 //Luces 
 
@@ -67,41 +67,62 @@ Array(200).fill().forEach(addStar)
 
 //Background
 
-const spaceTexture= new THREE.TextureLoader().load('galaxy.jpg')
-scene.background= spaceTexture;
+const spaceTexture = new THREE.TextureLoader().load('galaxy.jpg')
+scene.background = spaceTexture;
 
 
 
 //Convertir objetos en 2D en imagenes tridimencionales 
 //Cubo con mi foto
-const alanTexture= new THREE.TextureLoader().load('alan.png');
-const alan =new THREE.Mesh(
-    new THREE.BoxGeometry(3,3,3),
-    new THREE.MeshBasicMaterial({map:alanTexture})
+const alanTexture = new THREE.TextureLoader().load('alan.png');
+const alan = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.MeshBasicMaterial({ map: alanTexture })
 );
-//scene.add(alan)
+scene.add(alan)
+alan.position.z=10;
+alan.position.setX(10);
 
 
 //Moon
-
-const moonTexture= new THREE.TextureLoader().load('moon2.jpg')
+const moonTexture = new THREE.TextureLoader().load('moon2.jpg')
 //Esta linea se encarga de darle la ilusion de profundidad a la textura de la luna
-const normalTexture= new THREE.TextureLoader().load('normal.jpg')
-const moon= new THREE.Mesh(
-    new THREE.SphereGeometry(3,32,32),
+const normalTexture = new THREE.TextureLoader().load('normal.jpg')
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
     new THREE.MeshStandardMaterial({
         map: moonTexture,
         normalMap: normalTexture,
     })
 )
 scene.add(moon)
+//Posicionar la luna 
+//moon.position.z=6;
+//moon.position.setX(-20);
 
 
+//Funciones
+
+function moveCamera(){
+const t= document.body.getBoundingClientRect().top;
+moon.rotation.x += 0.05;
+moon.rotation.x += 0.075;
+moon.rotation.x += 0.05;
+
+alan.rotation.y += 0.01;
+alan.rotation.x += 0.01;
+
+camera.position.z = t * -0.01;
+camera.position.x = t * -0.0002;
+camera.rotation.y = t * -0.0002;
+
+}
+document.onscroll=moveCamera;
 function animate() {
     requestAnimationFrame(animate);
-    torus.rotation.x += 0.1;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+    alan.rotation.x += 0.1;
+    alan.rotation.y += 0.005;
+    alan.rotation.z += 0.01;
     //Detecta y actualiza la vista en base al cursor
     controls.update();
 
